a50a30a002a1a94700943a755fce6254e165d706
Leonardo Jardim <leojardim5@gmail.com>
2025-10-02 15:26:04 -0300

    chore: projeto inicial gerado pelo Spring Initializr

---
+++ src/main/java/br/unirio/tcc/gestao_tcc/GestaoTccApplication.java
@@ -0,0 +1,13 @@
+package br.unirio.tcc.gestao_tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unirio/tcc/gestao_tcc/GestaoTccApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unirio.tcc.gestao_tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
